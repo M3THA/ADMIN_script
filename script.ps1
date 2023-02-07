9ba538e13b4855fcbddb6b71c52e4cb21bc23994
Nicolas Gonzalez Farabollini <nicolasgf@outlook.com.ar>
2023-02-07 17:06:05 -0300

    compiled to run with powershell

--- script.ps1
+++ script.ps1
@@ -1,3 +1,36 @@
+﻿##[Ps1 To Exe]
+##
+##Kd3HDZOFADWE8uK1
+##Nc3NCtDXThU=
+##Kd3HFJGZHWLWoLaVvnQnhQ==
+##LM/RF4eFHHGZ7/K1
+##K8rLFtDXTiW5
+##OsHQCZGeTiiZ4tI=
+##OcrLFtDXTiW5
+##LM/BD5WYTiiZ49I=
+##McvWDJ+OTiiZ4tI=
+##OMvOC56PFnzN8u+Vs1Q=
+##M9jHFoeYB2Hc8u+Vs1Q=
+##PdrWFpmIG2HcofKIo2QX
+##OMfRFJyLFzWE8uK1
+##KsfMAp/KUzWJ0g==
+##OsfOAYaPHGbQvbyVvnQX
+##LNzNAIWJGmPcoKHc7Do3uAuO
+##LNzNAIWJGnvYv7eVvnQX
+##M9zLA5mED3nfu77Q7TV64AuzAgg=
+##NcDWAYKED3nfu77Q7TV64AuzAgg=
+##OMvRB4KDHmHQvbyVvnQX
+##P8HPFJGEFzWE8tI=
+##KNzDAJWHD2fS8u+Vgw==
+##P8HSHYKDCX3N8u+Vgw==
+##LNzLEpGeC3fMu77Ro2k3hQ==
+##L97HB5mLAnfMu77Ro2k3hQ==
+##P8HPCZWEGmaZ7/K1
+##L8/UAdDXTlaDjofG5iZk2WXnQWcnUuGUuqOqwY+o7NbcqSreW5sZSFd+NfryOFuyTvcARv0QuN1RRh44JuIf67zXJ/SqV6sahq16c+Du
+##Kc/BRM3KXxU=
+##
+##
+##fd6a9f26a06ea3bc99616d4851b372ba
 <#
 .NOTES
    Author      : Nicolas Gonzalez Farabollini
@@ -20,6 +53,7 @@ else {
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	choco install chocolatey-core.extension -y
     choco install googlechrome  vlc vcredist140 winrar dotnetfx ccleaner qbittorrent 7zip mpc-hc jre8 adobereader directx -y --ignore-checksums
-
-
 }
+
+$Shell = New-Object -ComObject "WScript.Shell"
+$Button = $Shell.Popup("Click OK para finalizar.", 0, "Hello", 0)
